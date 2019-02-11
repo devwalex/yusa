3f22c7b2a88b57cf785680ef631c7a741d358efa
Salami Usman Olawale <salamiusmanolawale@gmail.com>
2019-02-11 17:48:56 +0100

    Added adminSeeder

---
+++ database/seeds/AdminSeeder.js
@@ -0,0 +1,38 @@
+"use strict";
+
+/*
+|--------------------------------------------------------------------------
+| AdminSeeder
+|--------------------------------------------------------------------------
+|
+| Make use of the Factory instance to seed database with dummy data or
+| make use of Lucid models directly.
+|
+*/
+
+/** @type {import('@adonisjs/lucid/src/Factory')} */
+const Factory = use("Factory");
+const Database = use("Database");
+const Hash = use("Hash");
+class AdminSeeder {
+  async run() {
+    await Database.raw("SET FOREIGN_KEY_CHECKS = 0;");
+    await Database.truncate("users");
+    const encryptedPassword = await Hash.make("Yusa123");
+
+    const users = await Database.table("users").insert({
+      first_name: "Yusa",
+      last_name: "Admin",
+      email: "dev@yusa.org",
+      password: encryptedPassword,
+      phone_no: "08105636337",
+      gender: "Male",
+      address: "24, Turner Eradiri, Lagos",
+      username: "yusa",
+      is_verify: 1
+    });
+    await Database.raw("SET FOREIGN_KEY_CHECKS = 1;");
+  }
+}
+
+module.exports = AdminSeeder;
