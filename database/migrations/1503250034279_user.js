8f032541d95974849811c3ce0a0f5514f38337de
devwalex <salamiusmanolawale@gmail.com>
2020-03-05 16:46:58 +0100

    update user model and migration

--- database/migrations/1503250034279_user.js
+++ database/migrations/1503250034279_user.js
@@ -9,16 +9,24 @@ class UserSchema extends Schema {
       table.increments();
       table.string("first_name");
       table.string("last_name");
-      table.string("username", 80).notNullable().unique();
-      table.string("email", 254).notNullable().unique();
+      table
+        .string("username", 80)
+        .notNullable()
+        .unique();
+      table
+        .string("email", 254)
+        .notNullable()
+        .unique();
       table.string("phone_no");
       table.string("gender");
       table.string("address");
+      table.integer("user_role_id").defaultTo(2);
       table.string("password", 60).notNullable();
       table.boolean("is_verify").defaultTo(0);
       table.string("verification_token");
       table.boolean("is_login").defaultTo(0);
-      table.timestamps();
+      table.timestamp("created_at").defaultTo(this.fn.now());
+      table.timestamp("updated_at").defaultTo(this.fn.now());
     });
   }
 
