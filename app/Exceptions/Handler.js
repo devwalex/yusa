449cd8169b1c6cd699c09725b4f9a07c4247e7d2
devwalex <salamiusmanolawale@gmail.com>
2020-03-05 16:44:55 +0100

    create an error handler

---
+++ app/Exceptions/Handler.js
@@ -0,0 +1,46 @@
+"use strict";
+
+const BaseExceptionHandler = use("BaseExceptionHandler");
+
+/**
+ * This class handles all exceptions thrown during
+ * the HTTP request lifecycle.
+ *
+ * @class ExceptionHandler
+ */
+class ExceptionHandler extends BaseExceptionHandler {
+  /**
+   * Handle exception thrown during the HTTP lifecycle
+   *
+   * @method handle
+   *
+   * @param  {Object} error
+   * @param  {Object} options.request
+   * @param  {Object} options.response
+   *
+   * @return {void}
+   */
+  async handle(error, { request, response }) {
+    if (error.name === "InvalidJwtToken") {
+      return response.status(401).json({
+        success: false,
+        message: "Please provide a valid JWT token"
+      });
+    }
+    response.status(error.status).send(error.message);
+  }
+
+  /**
+   * Report exception for logging or debugging.
+   *
+   * @method report
+   *
+   * @param  {Object} error
+   * @param  {Object} options.request
+   *
+   * @return {void}
+   */
+  async report(error, { request }) {}
+}
+
+module.exports = ExceptionHandler;
